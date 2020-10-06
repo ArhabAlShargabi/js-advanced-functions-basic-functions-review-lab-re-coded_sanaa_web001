430f3ee53e1bb164ed96e74c4eb11e1c08929d33
Arhab Ahmed <arhab1000000@gmail.com>
2020-10-06 07:54:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-// Your code here
-let divide = () => 2000/100
-let square = x => x * x;
-let add = (a, b) => a + b;
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
+const Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b;
+  }
+}
+
+let actionApplyer = function(start, ray) {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++ ){
+    a = ray[i](a)
+  }
+
+  return a
+}
